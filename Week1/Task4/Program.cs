ec40f9c9fe4d54f9a0852dfc06ebb1456fa36e84
zhakha <47320638+zhakha@users.noreply.github.com>
2019-02-05 18:08:08 +0600

    Update Program.cs

--- Week1/Task4/Program.cs
+++ Week1/Task4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,17 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            // n - is the size of our 2d array
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
+            // for 2d array we will create and work with two cycles
         }
     }
 }
