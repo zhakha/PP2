632db51242600e25bc41feae8af697eea195c5bd
zhakha <47320638+zhakha@users.noreply.github.com>
2019-02-05 17:49:24 +0600

    Update Program.cs

--- Week1/Task3/Program.cs
+++ Week1/Task3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,18 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            // n - is the size of our array
+            int[] a = new int[n];
+            string s = Console.ReadLine();
+            a = s.Split(' ').Select(int.Parse).ToArray();
+            // we make the array with string s and call it how array- a[]
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+                Console.Write(a[i] + " ");
+            }
+            // in this cycle program will write the numbers with repeating
         }
     }
 }
