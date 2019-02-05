1ddaccca80c2176cdccae38d57c424c466b18281
zhakha <47320638+zhakha@users.noreply.github.com>
2019-02-05 17:39:08 +0600

    Update Program.cs

--- Week1/Task1/Program.cs
+++ Week1/Task1/Program.cs
@@ -1,15 +1,55 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1
+namespace ConsoleApp2
 {
     class Program
     {
+        public static bool IsPrime(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+            for(int i=2; i<=Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //Firstly, we must create a new function to find prime numbers
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            // n - is the size of our array
+            int[] a = new int[n];
+            string s = Console.ReadLine();
+            a = s.Split(' ').Select(int.Parse).ToArray();
+            // we make the array with string s and call it how array- a[]
+            int c = 0;
+            for (int i=0; i<a.Length; i++)
+            {
+                if (IsPrime(a[i]) == true)
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine(c);
+            // c will give us the count of prime numvers in our array
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (IsPrime(a[i]) == true)
+                {
+                    Console.Write(a[i] + " ");
+                }
+            }
+            // in this cycle program will write the prime numbers with using the function IsPrime
         }
     }
 }
