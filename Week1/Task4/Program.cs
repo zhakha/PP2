99b533013b31070946139de66a134ad0d4264b07
Zhakha <zhaksylyk.anesov@gmail.com>
2019-02-08 11:17:14 +0600

    upd

--- Week1/Task4/Program.cs
+++ Week1/Task4/Program.cs
@@ -10,6 +10,18 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            // n - is the size of our array
+            int[] a = new int[n];
+            string s = Console.ReadLine();
+            a = s.Split(' ').Select(int.Parse).ToArray();
+            // we make the array with string s and call it how array- a[]
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+                Console.Write(a[i] + " ");
+            }
+            // in this cycle program will write the numbers with repeating
         }
     }
 }
---
+++ Week2/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool IsPoli(string s)
+        {
+            if (s.Length <= 2)
+            {
+                return true;
+            }
+            string t = Reverse(s);
+            if (t == s)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static string Reverse(string x)
+        {
+            char[] c = x.ToCharArray();
+            Array.Reverse(c);
+            return new string(c);
+        }
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:/Users/user/Desktop/PP2lab2/1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string l = sr.ReadLine();
+            if (IsPoli(l) == true)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
