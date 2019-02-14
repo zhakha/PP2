f650b7da8a8065f3fafb4af13bcfa1977e29a36b
zhakha <47320638+zhakha@users.noreply.github.com>
2019-02-15 01:32:01 +0600

    Update Program.cs

--- Week1/Task4/Program.cs
+++ Week1/Task4/Program.cs
@@ -16,11 +16,6 @@ namespace Task4
             string s = Console.ReadLine();
             a = s.Split(' ').Select(int.Parse).ToArray();
             // we make the array with string s and call it how array- a[]
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.Write(a[i] + " ");
-                Console.Write(a[i] + " ");
-            }
             // in this cycle program will write the numbers with repeating
             // n - is the size of our 2d array
             for (int i = 0; i < n; i++)
